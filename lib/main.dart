a4b3b132221fb061e94a36ac3e55d5847d5ba2c6
Elias <alshowaiterelias@gmail.com>
2024-07-14 10:20:34 +0300

    connected with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:art_selling_platform/features/authentication/views/onBoarding/on
 import 'package:art_selling_platform/utils/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   //todo: bindings
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
   //todo: localStorage
   //todo: firebase
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepo()));
   runApp(const App());
 }
 
